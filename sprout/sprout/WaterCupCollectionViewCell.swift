31f970c7cc0835521550819184c85fd975496013
Yating <yz1339@nyu.edu>
2016-10-21 09:25:56 -0400

    inital water page working

---
+++ sprout/sprout/WaterCupCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  WaterCupCollectionViewCell.swift
+//  sprout
+//
+//  Created by Yating Zhan on 10/20/16.
+//  Copyright © 2016 Yating Zhan. All rights reserved.
+//
+
+import UIKit
+
+class WaterCupCollectionViewCell: UICollectionViewCell {
+    
+}
--- sprout/sprout/WaterViewController.swift
+++ sprout/sprout/WaterViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 
 class WaterViewController: UIViewController {
 
+    @IBOutlet weak var cup1: UIButton!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func drink(_ sender: UIButton) {
+//        let #imageLiteral(resourceName: "watercupGrey") = UIImage(named: "waterCupGrey")
+//        let #imageLiteral(resourceName: "watercupBlue") = UIImage(named: "waterCupBlue")
+        if sender.currentImage == #imageLiteral(resourceName: "watercupBlue"){
+            sender.setImage(#imageLiteral(resourceName: "watercupGrey"), for: UIControlState.normal)
+        }else{
+            sender.setImage(#imageLiteral(resourceName: "watercupBlue"), for: UIControlState.normal)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
