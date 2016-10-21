b178fab426e334e89b197e7cfd6b3a04b11ebee8
Yating <yz1339@nyu.edu>
2016-10-20 20:10:04 -0400

    initial commit with home page

---
+++ sprout/sprout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sprout
+//
+//  Created by Yating Zhan on 10/20/16.
+//  Copyright © 2016 Yating Zhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
